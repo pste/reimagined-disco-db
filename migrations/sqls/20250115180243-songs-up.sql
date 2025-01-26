d5696d329aeb32821463c9f527144f8db533a93c
pste <pirraste@gmail.com>
2025-01-26 18:30:19 +0100

    [not null] everywhere

--- migrations/sqls/20250115180243-songs-up.sql
+++ migrations/sqls/20250115180243-songs-up.sql
@@ -3,35 +3,35 @@
 -- **
 CREATE TABLE artists (
   artist_id SERIAL PRIMARY KEY,
-  "name" varchar,
+  "name" varchar NOT NULL,
   UNIQUE("name")
 );
 
 -- **
 CREATE TABLE albums (
   album_id SERIAL PRIMARY KEY,
-  title varchar,
-  artist_id INT REFERENCES artists,
-  "year" int,
-  genre varchar,
+  title varchar NOT NULL,
+  artist_id INT NOT NULL REFERENCES artists,
+  "year" int default(0) NOT NULL,
+  genre varchar NOT NULL,
   UNIQUE(title, artist_id)
 );
 
 -- **
 CREATE TABLE songs (
   song_id SERIAL PRIMARY KEY,
-  title varchar,
-  track_nr int,
-  disc_nr int default(1),
-  album_id INT REFERENCES albums,
+  title varchar NOT NULL,
+  track_nr int default(1) NOT NULL,
+  disc_nr int default(1) NOT NULL,
+  album_id INT  NOT NULL REFERENCES albums,
   UNIQUE(title, album_id)
 );
 
 -- **
 CREATE TABLE files (
   song_id SERIAL PRIMARY KEY REFERENCES songs,
-  file_path varchar,
-  file_name varchar,
-  modified timestamp, -- no time zone
+  file_path varchar NOT NULL,
+  file_name varchar NOT NULL,
+  modified timestamp NOT NULL, -- no time zone
   UNIQUE(file_path, file_name)
 );
