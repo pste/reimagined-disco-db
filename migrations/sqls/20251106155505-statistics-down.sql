2b4f081d93b38818563941101763e3d8e458d4c9
pste <pirraste@gmail.com>
2025-11-07 14:44:22 +0100

    [stats] fix stats for users

--- migrations/sqls/20251106155505-statistics-down.sql
+++ migrations/sqls/20251106155505-statistics-down.sql
@@ -1,2 +1,4 @@
 -- **
-DROP TABLE IF EXISTS statistics;
+DROP TABLE IF EXISTS user_stats;
+-- **
+ALTER TABLE files DROP COLUMN created;
--- migrations/sqls/20251106155505-statistics-up.sql
+++ migrations/sqls/20251106155505-statistics-up.sql
@@ -1,10 +1,20 @@
 -- **
-CREATE TABLE statistics (
-  song_id SERIAL PRIMARY KEY REFERENCES songs,
-  added timestamp NOT NULL,
+CREATE TABLE user_stats (
+  user_stats_id SERIAL PRIMARY KEY,
+  song_id SERIAL REFERENCES "songs",
+  user_id SERIAL REFERENCES "users",
   played timestamp NULL,
   stars INT DEFAULT(0)
 );
 
-INSERT INTO statistics(song_id, added) 
-  SELECT song_id, current_date FROM songs;
+-- ** ADD CREATED
+ALTER TABLE files ADD COLUMN created timestamp DEFAULT NOW();
+UPDATE files SET created = modified;
+ALTER TABLE files ALTER COLUMN created SET NOT NULL;
+
+-- ** MOVE modified COL TO THE END
+ALTER TABLE files RENAME COLUMN modified TO modified_old;
+ALTER TABLE files ADD COLUMN modified timestamp;
+UPDATE files SET modified = modified_old;
+ALTER TABLE files ALTER COLUMN modified SET NOT NULL;
+ALTER TABLE files DROP COLUMN modified_old;
